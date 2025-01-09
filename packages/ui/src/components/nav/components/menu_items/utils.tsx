5f11ec67b72bc36493ed33e69dffa0030bf1f082
ramiqadoumi <108194735+ramiqadoumi@users.noreply.github.com>
2025-01-09 19:49:43 +0300

    remove staking page

--- packages/ui/src/components/nav/components/menu_items/utils.tsx
+++ packages/ui/src/components/nav/components/menu_items/utils.tsx
@@ -5,7 +5,6 @@ import ProposalsIcon from 'shared-utils/assets/icon-proposals.svg';
 import TransactionIcon from 'shared-utils/assets/icon-transaction.svg';
 import UserIcon from 'shared-utils/assets/icon-user.svg';
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 import {
   BLOCKS,
   FAUCET,
@@ -14,7 +13,6 @@ import {
   PROPOSALS,
   TRANSACTIONS,
   VALIDATORS,
-  STAKING,
 } from '@/utils/go_to_page';
 
 export const getMenuItems = () => {
@@ -58,11 +56,6 @@ export const getMenuItems = () => {
       key: 'faucet',
       url: FAUCET,
       icon: <AttachMoneyIcon {...iconProps} />,
-    },
-    {
-      key: 'staking',
-      url: STAKING,
-      icon: <MonetizationOnIcon {...iconProps} />,
     },
   ];
 };
